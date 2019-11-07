2cac256efdd1536a0c86ea0ceeb16faa31fdd71b
pbj <pbj@pop-os.localdomain>
2019-11-06 21:41:26 -0500

    Added initial page Added .gitignore

--- App/PrototypeI/App.js
+++ App/PrototypeI/App.js
@@ -1,22 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 import {TextInput} from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
 
-  const [value, onChangeText] = React.useState(' Enter Bill Total Inlcuding Tax+Tip ');
+  // const [total, onChangeTextTotal] = React.useState('');
+  // const [people, onChangeTextPeople] = React.useState('');
  
-  return (
-    <View style={styles.centeralign}>
-      <Text style ={styles.heading} > BillSplitter Prototype</Text>
-      <Text style ={styles.text} > This application will help you divide your bill invidiually amongst your people</Text>
-      <TextInput
-  style={{ height: 30, borderColor: 'gray', borderWidth: 3 }}
-  onChangeText={text => onChangeText(text)}
-  value={value} />
-    </View>
-  );
+  constructor(props){
+    super(props)
+    this.state = {
+      total: 0,
+      people: 1,
+    };
+  };
+
+  render(){
+
+    return (
+      <View style={styles.centeralign}>
+        <Text style ={styles.heading} > BillSplitter Prototype</Text>
+        <Text style ={styles.text} > This application will help you divide your bill invidiually amongst your people</Text>
+        
+          <TextInput
+            style={styles.inputBox}
+            // onChangeText={text => onChangeTextTotal(text)}
+            onChangeText={total => this.setState({total})}
+            // value={this.state.total} 
+            placeholder='Enter Total + Tip'
+            keyboardType='number-pad'/>
+          
+          <TextInput
+            style={styles.inputBox}
+            // onChangeText={text => onChangeTextPeople(text)}
+            onChangeText={people => this.setState({people})}
+            // value={this.state.people} 
+            placeholder='Number of People'
+            keyboardType='number-pad'/> 
+
+          <Text style ={styles.heading} >{this.state.total/this.state.people} </Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -30,10 +56,20 @@ const styles = StyleSheet.create({
     color: 'red',
     fontWeight: 'bold',
     fontSize: 30,
+    textAlign: 'center'
   },
   text: {
     color: 'black',
     fontSize: 15,
     textAlign: 'center'
+  },
+  inputBox: {
+    height: 30, 
+    borderColor: 'black',
+    borderBottomWidth: 1,
+    width: '30%',
+    textAlign: 'center'
   }
 });
+
+    
