e16282eb533e0131bc960ded679bef0b260c34a0
Hamid Ghasemi <ghasemih@mcmaster.ca>
2020-01-21 23:33:46 -0400

    Update

--- App/PrototypeI/App.js
+++ App/PrototypeI/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import { TextInput, StyleSheet, Text, View, Button, Image } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import RNTesseractOcr from 'react-native-tesseract-ocr';
-import * as ImagePicker from 'expo-image-picker';
-import Constants from 'expo-constants';
-import * as Permissions from 'expo-permissions';
+import ProfileScreen from '../PrototypeI/ProfileScreen';
 
 class HomeScreen extends React.Component {
-
   constructor(props) {
     super(props)
     this.state = {
@@ -25,18 +21,21 @@ class HomeScreen extends React.Component {
   render() {
     return (
       <View style={styles.centeralign}>
-        <Button
-          title="Go to new page \"
-          //helps in navigation to different screens
-          onPress={() => this.props.navigation.navigate('Profile',
-            {
-              total: 10,
-              name: 'ibrahim',
-            }
-          )}
-        />
-        <Text style={styles.heading} > BillSplitter Prototype</Text>
-        <Text style={styles.text} > This application will help you divide your bill invidiually amongst your people</Text>
+        <Text style={styles.heading} > BillSplitter </Text>
+        <Text style={styles.text} > Provide the information or add a photo</Text>
+        
+        <View style={styles.button}>
+          <Button 
+            title="Add photo"
+            //helps in navigation to different screens
+            onPress={() => this.props.navigation.navigate('Profile',
+              {
+                total: 10,
+                name: 'ibrahim',
+              }
+            )}
+          />
+        </View>
 
         <TextInput
           style={styles.inputBox}
@@ -55,81 +54,6 @@ class HomeScreen extends React.Component {
           keyboardType='number-pad' />
 
         <Text style={styles.heading} >{this.state.total / this.state.people} </Text>
-      </View>
-    );
-  }
-}
-
-class ProfileScreen extends React.Component {
-
-  state = {
-    image: null,
-  };
-
-  static navigationOptions = {
-    title: 'ProfileScreen\t',
-  };
-
-  //Asks for permissions
-  componentDidMount() {
-    this.getPermissionAsync();
-  }
-
-  //Need permission to access camera roll for ios
-  getPermissionAsync = async () => {
-    if (Constants.platform.ios) {
-      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-      if (status !== 'granted'){
-        alert('Sorry, need camera roll permissions');
-        console.log('Permission to camera roll not granted');
-      }
-    }
-  }
-
-  //Function that lets user pick an image
-  _pickImage = async () => {
-    let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ImagePicker.MediaTypeOptions.All,
-      allowsEditing: true,
-      aspect: [4,3],
-    });
-
-    console.log(result);
-
-    if (!result.cancelled){
-      this.setState({image: result.uri});
-    }
-  };
-
-  
-  render() {
-    // Needed for accessing parameters from previous screen
-    const {navigation} = this.props;
-    let {image} = this.state;
-    return (
-      <View style={styles.centeralign}>
-
-        <Button
-          title='Go to Welcome page'
-          //helps in navigation to different screens
-          onPress={() => this.props.navigation.navigate('Home')}
-        />
-
-        <Text style={styles.heading} > BillSplitter PAGE 2 BABY</Text>
-        {/* <Text style={styles.text} > This application will help you divide your bill invidiually amongst your people</Text> */}
-        <Text style={styles.heading}>{JSON.stringify(navigation.getParam('name','default value'))}</Text>
-        <Button
-          title="Pick_Image\"
-          //helps in navigation to different screens
-          onPress={this._pickImage}
-        />
-        <View style={styles.ImageContainer}>    
-{image &&
-  <Image source={{ uri: image }} style={{ width: 250, height: 250}} />}
-
-        </View>
-
-
       </View>
     );
   }
@@ -210,11 +134,16 @@ const styles = StyleSheet.create({
     color: 'red',
     fontWeight: 'bold',
     fontSize: 30,
+    marginBottom: 30,
     textAlign: 'center'
+  },
+  button:{
+    marginBottom: 20,
   },
   text: {
     color: 'black',
-    fontSize: 15,
+    fontSize: 20,
+    marginBottom: 100,
     textAlign: 'center'
   },
   inputBox: {
@@ -234,8 +163,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#CDDC39'
   }
 });
-
-
 
 
 // /**
---
+++ App/PrototypeI/ProfileScreen.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { TextInput, StyleSheet, Text, View, Button, Image } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+
+export default class ProfileScreen extends React.Component {
+
+    state = {
+      image: null,
+    };
+  
+    static navigationOptions = {
+      title: 'ProfileScreen\t',
+    };
+  
+    //Asks for permissions
+    componentDidMount() {
+      this.getPermissionAsync();
+    }
+  
+    //Need permission to access camera roll for ios
+    getPermissionAsync = async () => {
+      if (Constants.platform.ios) {
+        const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+        if (status !== 'granted'){
+          alert('Sorry, need camera roll permissions');
+          console.log('Permission to camera roll not granted');
+        }
+      }
+    }
+  
+    //Function that lets user pick an image
+    _pickImage = async () => {
+      let result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.All,
+        allowsEditing: true,
+        aspect: [4,3],
+      });
+  
+      console.log(result);
+  
+      if (!result.cancelled){
+        this.setState({image: result.uri});
+      }
+    };
+  
+    
+    render() {
+      // Needed for accessing parameters from previous screen
+      const {navigation} = this.props;
+      let {image} = this.state;
+      return (
+        <View style={styles.centeralign}>
+  
+          <Button
+            title='Go to Welcome page'
+            //helps in navigation to different screens
+            onPress={() => this.props.navigation.navigate('Home')}
+          />
+  
+          <Text style={styles.heading} > BillSplitter PAGE 2 BABY</Text>
+          {/* <Text style={styles.text} > This application will help you divide your bill invidiually amongst your people</Text> */}
+          <Text style={styles.heading}>{JSON.stringify(navigation.getParam('name','default value'))}</Text>
+          <Button
+            title="Pick_Image\"
+            //helps in navigation to different screens
+            onPress={this._pickImage}
+          />
+          <View style={styles.ImageContainer}>    
+  {image &&
+    <Image source={{ uri: image }} style={{ width: 250, height: 250}} />}
+  
+          </View>
+  
+  
+        </View>
+      );
+    }
+}
+
+
+const styles = StyleSheet.create({
+    centeralign: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    heading: {
+      color: 'red',
+      fontWeight: 'bold',
+      fontSize: 30,
+      textAlign: 'center'
+    },
+    text: {
+      color: 'black',
+      fontSize: 15,
+      textAlign: 'center'
+    },
+    inputBox: {
+      height: 30,
+      borderColor: 'black',
+      borderBottomWidth: 1,
+      width: '30%',
+      textAlign: 'center'
+    },
+    ImageContainer: {
+      borderRadius: 1,
+      width: 250,
+      height: 250,
+      borderColor: '#9B9B9B',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#CDDC39'
+    }
+  });
